8be5c3fc8965655d6687b19f6b2c0181dfe82ddf
MitaliO <91545161+MitaliO@users.noreply.github.com>
2021-12-17 15:25:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const { rsort } = require('semver')
 //const bodyParser = require('body-parser')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/annotations', { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
